0acb977b6a5ae8f4eb05a219e56657c56bfcf6a9
VIRAJ261-cloud <darkknightthe91@gmail.com>
2025-12-28 11:22:22 +0530

    Month wise comparision

--- src/services/expenseService.js
+++ src/services/expenseService.js
@@ -44,6 +44,93 @@ export const getUserExpenses = async () => {
   }));
 };
 
+export const getUserExpensesByMonth = async (monthNumber, yearNumber) => {
+  const user = auth.currentUser;
+  if (!user) return null;
+
+  const q = query(
+    collection(db, "expenses"),
+    where("userId", "==", user.uid)
+  );
+  const snapshot = await getDocs(q);
+  const expenses = snapshot.docs.map((doc) => doc.data());
+  let monthTotal = expenses.filter((exp) => {
+    let dateOfExpense = exp.date.split('-')
+    let monthN = parseInt(dateOfExpense[1])
+    let yearN = parseInt(dateOfExpense[0])
+    return ((monthN == monthNumber) && (yearN == yearNumber))
+  }).reduce((sum, exp) => {
+    return sum = sum + exp.amount
+  }, 0)
+  return monthTotal
+}
+
+export const getPrevMonthAndYear  = (currentMonthNumber,currentYear) =>{
+  let prevMonth = 0
+  let prevYear = 0
+  if(currentMonthNumber == 1){
+    prevMonth = 12
+    prevYear = currentYear - 1
+    return {
+      prevMonth,
+      prevYear
+    };
+  }
+  prevMonth = currentMonthNumber-1
+  prevYear = currentYear
+  return {
+    prevMonth,
+    prevYear
+  }
+}
+
+export const compareCurrAndPrev = async (
+  currentMonthNumber,
+  currentYear
+) => {
+  try {
+    const { prevMonth, prevYear } = getPrevMonthAndYear(
+      currentMonthNumber,
+      currentYear
+    );
+
+    const prevExpenses =
+      (await getUserExpensesByMonth(prevMonth, prevYear)) ?? 0;
+
+    const currExpenses =
+      (await getUserExpensesByMonth(currentMonthNumber, currentYear)) ?? 0;
+
+    // No spending in previous month
+    if (prevExpenses === 0) {
+      return {
+        percentageChange: null,
+        message: "No expenses in previous month",
+        current: currExpenses,
+        previous: prevExpenses,
+      };
+    }
+
+    const percentageChange =
+      ((currExpenses - prevExpenses) / prevExpenses) * 100;
+
+    return {
+      percentageChange: Number(percentageChange.toFixed(2)),
+      message:
+        percentageChange > 0
+          ? "Spending increased"
+          : percentageChange < 0
+          ? "Spending decreased"
+          : "No change in spending",
+      current: currExpenses,
+      previous: prevExpenses,
+    };
+  } catch (error) {
+    console.error("Error comparing expenses:", error);
+    throw new Error("Failed to compare monthly expenses");
+  }
+};
+
+
 // DASHBOARD STATS
 export const getDashboardStats = async () => {
   const user = auth.currentUser;
@@ -57,20 +144,15 @@ export const getDashboardStats = async () => {
   const snapshot = await getDocs(q);
   const expenses = snapshot.docs.map((doc) => doc.data());
 
-  const today = new Date().toISOString().split("T")[0];
-
-  let todayTotal = 0;
-  let totalAmount = 0;
-
-  expenses.forEach((exp) => {
-    totalAmount += exp.amount;
-    if (exp.date === today) {
-      todayTotal += exp.amount;
-    }
-  });
+  const today1 = new Date()
+  // const today = today1.toISOString().split("T")[0];
+  let totalAmount = expenses.reduce((sum, exp) => {
+    return sum = sum + exp.amount
+  }, 0)
+  let monthTotal = await getUserExpensesByMonth(today1.getMonth() + 1, today1.getFullYear())
 
   return {
-    todayTotal,
+    monthTotal,
     totalAmount,
     totalOrders: expenses.length,
   };
